57bb6882c7f6986ea9627a3560d1cc1f7f929daa
Adevanes Rosa <celebratte@gmail.com>
2022-08-27 13:57:19 -0300

    update cadastro

--- cypress/integration/Cadastro.spec.js
+++ cypress/integration/Cadastro.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
-
 //Caminho feliz
-context('Testes de funcionalidade cadastro bugereats', () => {
+
+context('Testes de funcionalidade cadastro Bugereats', () => {
     
     it('Cadastro válido', () => {
     cy.visit('https://buger-eats.vercel.app/')
@@ -16,10 +16,16 @@ context('Testes de funcionalidade cadastro bugereats', () => {
     cy.get(':nth-child(4) > :nth-child(1) > input').type('92')
     cy.get(':nth-child(4) > :nth-child(2) > input').type('casa')
     cy.get('.delivery-method > :nth-child(2)').click()
-    const fixtureFile = 'cnh.jpg';
-    const fileInputElement = ".dropzone";
+   
+    
+    //cy.get('.dropzone')
+    //.attachFile('cnh.jpg');
+
+
+    /* const fixtureFile = 'cnh.jpg';
+    const fileInputElement = "p";
     cy.get(fileInputElement).attachFile(fixtureFile)
-    cy.get('.button-success').click() 
+    cy.get('.button-success').click() */ 
 
 });
 
