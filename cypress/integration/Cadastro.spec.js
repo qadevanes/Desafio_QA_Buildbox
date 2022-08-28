ffe0f7b44745d15b5641a7df6585ab86ea8409bb
Adevanes Rosa <celebratte@gmail.com>
2022-08-28 01:25:00 -0300

    Update Cadastro.spec.js

--- cypress/integration/Cadastro.spec.js
+++ cypress/integration/Cadastro.spec.js
@@ -4,7 +4,7 @@ import auth from '../support/actions/authActions'
 context('Testes de funcionalidade cadastro Bugereats', () => {
     
     it('Cadastro válido', () => {
-        
+    //Caminho feliz   
     auth.go()
     cy.get('a').click()
     cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
@@ -16,17 +16,30 @@ context('Testes de funcionalidade cadastro Bugereats', () => {
     cy.get(':nth-child(4) > :nth-child(1) > input').type('92')
     cy.get(':nth-child(4) > :nth-child(2) > input').type('casa')
     cy.get('.delivery-method > :nth-child(2)').click()
-    //esse seria o codigo para fazer o upload
-    //const yourFixturePath = 'cnh.jpg';
-    cy.get('.dropzone').attachFile('../fixtures/cnh.jpg');
-   
-    /* const fixtureFile = 'cnh.jpg';
-    const fileInputElement = "p";
-    cy.get(fileInputElement).attachFile(fixtureFile)
-    cy.get('.button-success').click() */ 
+    cy.get('.dropzone>input').attachFile('cnh.jpg');
+    cy.get('.button-success').click()
+    cy.get('#swal2-title').should('have.text', 'Aí Sim...')
 
 });
 
-
-
+    //Cenários negativos
+    it('Cad', () => {
+        //Caminho feliz   
+        auth.go()
+        cy.get('a').click()
+        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
+        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > input').type('01829747070')
+        cy.get(':nth-child(3) > :nth-child(1) > input').type('qadevanes@gmail.com')
+        cy.get(':nth-child(3) > :nth-child(2) > input').type('51980268109')
+        cy.get(':nth-child(3) > :nth-child(2) > :nth-child(1) > input').type('91740460')
+        cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2) > input').click()
+        cy.get(':nth-child(4) > :nth-child(1) > input').type('92')
+        cy.get(':nth-child(4) > :nth-child(2) > input').type('casa')
+        cy.get('.delivery-method > :nth-child(2)').click()
+        cy.get('.dropzone>input').attachFile('cnh.jpg');
+        cy.get('.button-success').click()
+        cy.get('#swal2-title').should('have.text', 'Aí Sim...')
+    
+    });
+    
 });
