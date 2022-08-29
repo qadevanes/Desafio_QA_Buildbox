74b90707176a4931db88ea790123a55f1e7a1d3c
Adevanes Rosa <celebratte@gmail.com>
2022-08-29 08:11:38 -0300

    Update authActions.js

--- cypress/support/actions/authActions.js
+++ cypress/support/actions/authActions.js
@@ -3,7 +3,7 @@ class AuthAction{
 //Acesso a página    
 go() {
  //Teste de cenarios responsivo console / exemplo: 'iphone-xr', 'sansung-s10' , '1920, 1080'
- cy.viewport('iphone-xr')
+ cy.viewport('1440,900')
  cy.visit('https://buger-eats.vercel.app/')
 }
 
