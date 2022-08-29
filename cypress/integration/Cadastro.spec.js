4be0fad9097de879a7d37c8c7d5b1a4c7e4ed1f5
Adevanes Rosa <celebratte@gmail.com>
2022-08-29 10:12:15 -0300

    update Cadastro

--- cypress/integration/Cadastro.spec.js
+++ cypress/integration/Cadastro.spec.js
@@ -4,65 +4,42 @@ import auth from '../support/actions/authActions'
 context('Testes de funcionalidade cadastro Bugereats', () => {
     
     it('Cadastro válido', () => {
-    // #Cenário feliz   
+    // #Cenário feliz 
+
     auth.go()
-    cy.get('a').click()
-    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
-    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > input').type('01829747070')
-    cy.get(':nth-child(3) > :nth-child(1) > input').type('qadevanes@gmail.com')
-    cy.get(':nth-child(3) > :nth-child(2) > input').type('51980268109')
-    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(1) > input').type('91740460')
-    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2) > input').click()
-    cy.get(':nth-child(4) > :nth-child(1) > input').type('92')
-    cy.get(':nth-child(4) > :nth-child(2) > input').type('casa')
-    cy.get('.delivery-method > :nth-child(2)').click()
-    cy.get('.dropzone>input').attachFile('cnh.jpg');
-    cy.get('.button-success').click()
-    cy.get('#swal2-title').should('have.text', 'Aí Sim...')
+    auth.cadastroParceiroSucesso()
 
 });
 
     // #Cenários negativos
     it('Cadastro com nome incompleto', () => {
           
-        auth.go()
-        cy.get('a').click()
-        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes')
-        cy.log('Deve exibir uma mensagem de alerta: "Digite seu nome completo"')
+    auth.go()
+    auth.cadastroNomeIncompleto()
     
     });
 
       
-      it('Cadastro com nome input vazio', () => {
+    it('Cadastro com nome input vazio', () => {
           
-        auth.go()
-        cy.get('a').click()
-        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('a').clear()
-        cy.get('.button-success').click()
-        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > .alert-error').should('have.text', 'É necessário informar o nome')
+    auth.go()
+    auth.cadastroNomeInputVazio()
         
     
     });
     
     it('Cadastro com CPF input vazio', () => {
           
-        auth.go()
-        cy.get('a').click()
-        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
-        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > input').type('0').clear()
-        cy.get('.button-success').click()
-        cy.get(':nth-child(2) > :nth-child(2) > .alert-error').should('have.text', 'É necessário informar o CPF')
+     auth.go()
+     auth.cadastroCpfInputVazio()
+     
     
     });
 
     it('Cadastro com CPF inválido', () => {
          
         auth.go()
-        cy.get('a').click()
-        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
-        cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > input').type('huhauhauha')
-        cy.get('.button-success').click()
-        cy.get(':nth-child(2) > :nth-child(2) > .alert-error').should('have.text', 'Oops! CPF inválido')
+        
         
     
     });
@@ -193,7 +170,6 @@ context('Testes de funcionalidade cadastro Bugereats', () => {
         cy.get('.delivery-method > :nth-child(2)').click()
         cy.get('.button-success').click()
         cy.get('form > :nth-child(6)').should('contain','Adicione uma foto da sua CNH')
-        
     
     });
 });
--- cypress/support/actions/authActions.js
+++ cypress/support/actions/authActions.js
@@ -2,10 +2,61 @@ class AuthAction{
  
 //Acesso a página    
 go() {
- //Teste de cenarios responsivo console / exemplo: 'iphone-xr', 'sansung-s10' , '1920, 1080'
- cy.viewport('1440,900')
+ //Teste de cenarios responsivo console / exemplo: 'iphone-xr', 'sansung-s10'
+ cy.viewport('macbook-15')
  cy.visit('https://buger-eats.vercel.app/')
 }
+
+// Cenário feliz
+cadastroParceiroSucesso () {
+    cy.get('a').click()
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > input').type('01829747070')
+    cy.get(':nth-child(3) > :nth-child(1) > input').type('qadevanes@gmail.com')
+    cy.get(':nth-child(3) > :nth-child(2) > input').type('51980268109')
+    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(1) > input').type('91740460')
+    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2) > input').click()
+    cy.get(':nth-child(4) > :nth-child(1) > input').type('92')
+    cy.get(':nth-child(4) > :nth-child(2) > input').type('casa')
+    cy.get('.delivery-method > :nth-child(2)').click()
+    cy.get('.dropzone>input').attachFile('cnh.jpg');
+    cy.get('.button-success').click()
+    cy.get('#swal2-title').should('have.text', 'Aí Sim...')
+}
+
+// #Cenários negativos
+cadastroNomeIncompleto(){
+    cy.get('a').click()
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes')
+    cy.log('Deve exibir uma mensagem de alerta: "Digite seu nome completo"')
+
+}
+
+cadastroNomeInputVazio(){
+    cy.get('a').click()
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('a').clear()
+    cy.get('.button-success').click()
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > .alert-error').should('have.text', 'É necessário informar o nome')
+}
+
+cadastroCpfInputVazio(){
+    cy.get('a').click()
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > input').type('0').clear()
+    cy.get('.button-success').click()
+    cy.get(':nth-child(2) > :nth-child(2) > .alert-error').should('have.text', 'É necessário informar o CPF')
+}
+
+cadastroCpfInválido(){
+    cy.get('a').click()
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > input').type('Adevanes Thiago')
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > input').type('huhauhauha')
+    cy.get('.button-success').click()
+    cy.get(':nth-child(2) > :nth-child(2) > .alert-error').should('have.text', 'Oops! CPF inválido')   
+
+}
+
+
 
 }
 
